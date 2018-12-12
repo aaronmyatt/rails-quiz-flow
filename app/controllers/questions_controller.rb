af035e750374a5b7ad1cf7f63f2c920dcfdec94f
Mindvalley <mindvalley@96F9VM-MacPro-KLS.local>
2018-12-12 16:00:23 +0800

    Given I have a question form |> when I answer and submit the form |> Then I am sent to the recommendation screen

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,9 @@
 class QuestionsController < ApplicationController
   def index
   end
+
+  def form
+    redirect_to "/recommendation/index"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'category#index'
   get 'questions/index'
+  post 'questions/form'
   get 'drilldown/index'
   get 'category/index'
+  get 'recommendation/index'
 end
