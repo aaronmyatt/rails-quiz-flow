9a87e7250fd56e325665f72ddfa737616cd9c54e
ProsperMindvalley <45549649+ProsperMindvalley@users.noreply.github.com>
2018-12-19 14:45:51 +0800

    Prosper (#1)
    * Added the breadcrumb
    
    * Category and Drilldown Menus Modified
    
    * Deleted The Shared Google Folder as it is no longer needed
    
    * Added Drilldown Columns & a few housekeeping

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,5 +1,11 @@
 class CategoryController < ApplicationController
   def index
+
+    @columns1 = [1,2,3,4]
+    @columns2 = [5,6,7,8]
+    @columns3 = [9,10,11,12]
+    @columns4 = [13,14,15,16]
+    
   end
 
   def form
--- app/controllers/drilldown_controller.rb
+++ app/controllers/drilldown_controller.rb
@@ -1,11 +1,18 @@
 class DrilldownController < ApplicationController
   def index
-    @drilldown = []
-    session[:category].each do |category|
-      for i in 1..4 do
-        @drilldown.push("#{category}drilldown#{i}")
+
+      drilldown = []
+      session[:category].each do |category|
+        for i in 1..4 do
+          drilldown.push("#{category}drilldown#{i}")
+        end
       end
-    end
+
+       @drilldown1 = drilldown[0..3]
+       @drilldown2 =drilldown[4..7]
+       @drilldown3 =drilldown[8..11]
+       @drilldown4 =drilldown[12..15]
+       
   end
 
   def form
