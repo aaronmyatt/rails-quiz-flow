d9bc0c9a831e540c036ff4e18f67a97c04448159
ProsperMindvalley <45549649+ProsperMindvalley@users.noreply.github.com>
2018-12-21 11:00:07 +0800

    Implemented Toggle-able category and drilldown buttons (#3)
    * Implemented Toggle-able buttons and Questions Drilldown Columns
    
    * Category and Drilldown label tags updated

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,16 @@
 class QuestionsController < ApplicationController
   def index
-    @question=[]
+    question=[]
     session[:drilldown].each do |drilldown|
       for i in 1..2 do
-        @question.push("#{drilldown}question#{i}")
+        question.push("#{drilldown}question#{i}")
       end
     end
+    @questions1 = question[0..1]
+    @questions2 = question[2..3]
+    @questions3 = question[4..5]
+    @questions4 = question[6..7]
+
   end
 
   def form
